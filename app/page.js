127596e4a084a46c8ebc94affacad6a1bae3de3c
Joao Astolfi <jpastolfi@gmail.com>
2023-11-14 00:20:42 +0000

    Text correction and some small changes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
@@ -16,9 +17,9 @@ export default function Home() {
     <div className={darkMode && 'dark'}>
       <main className='bg-white-950 px-10 md-px-20 lg:px-40 dark:bg-gray-950'>
         <section className="min-h-screen">
-          <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons dark:text-gray-100'>developed by jp</h1>
-            <ul className='flex items-center'>
+          <nav className='py-10 flex justify-center'>
+            {/* <h1 className='text-xl font-burtons dark:text-gray-100'>developed by me</h1> */}
+            <ul className='flex items-center justify-between'>
               <li><BsFillMoonStarsFill onClick = {() => setDarkMode(!darkMode)}className='cursor-pointer text-2xl dark:text-gray-200' /></li>
               <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="/João Astolfi CV.pdf" download="Joao Astolfi CV" target="_blank" rel="nonreferrer">Download CV</a></li>
             </ul>
@@ -26,7 +27,7 @@ export default function Home() {
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>João Astolfi</h2>
             <h3 className='text-2xl py-2 md:text-3xl dark:text-gray-200'>Developer</h3>
-            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>Fullstack developer but I actually prefer backend lol</p>
+            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>Hi there! I am a fullstack developer mainly focused in React and Node.js, but I'm currently studying Java and C#.</p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
             <a href='https://www.linkedin.com/in/jpastolfi/' target='_blank'><AiFillLinkedin /></a>
@@ -38,72 +39,67 @@ export default function Home() {
         </section>
         <section className='text-center'>
           <div>
-            <h3 className='text-3xl py-1 dark:text-gray-200'>Services I offer</h3>
+            <h3 className='text-3xl py-1 text-teal-600 dark:text-teal-600'>About me</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I have done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              I graduated as a Deck Officer in 2016 and spent 6 years at sea, sailing along the coast of Brazil. During this period I worked alongside crewmembers from Asia, Europe and South America, first as a life-saving appliances officer and later as a navigation officer. <br></br>
             </p>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
-              Yeah yeah I am just going to write a lot of random things in here because apparently my lorem is not working so I have to do it manually.
+              After these years at sea I decided to make a carreer change so that I could spend more time close to my family and friends. I decided to start studying web development and completed three modules in Trybe, a Brazilian web development school, on the following topics:
+            </p>
+            <ol className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              <li><span className='font-bold text-teal-600'>Basics on web development:</span> Unix & Bash, Git, HTML & CSS, JS & DOM, Unit Testing</li><br></br>
+              <li><span className='font-bold text-teal-600'>Front-end development:</span> React, including components, events and state, controlled components, lifecycle, router, tests with RTL, Redux, context API and React hooks.</li><br></br>
+              <li><span className='font-bold text-teal-600'>Back-end development:</span> Docker, MySQL, Node.js & Express, unit and integration testing w/ Mocha, Chai and Sinon, Typescript, layered architecture, REST, JWT, ORM with Sequelize, deployment, OOP and SOLID principles</li>
+            </ol>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Currently I'm studying Java and C#, mainly focused in Spring Boot and ASP .Net, respectively.
             </p>
           </div>
+          <h3 className='text-3xl py-1 text-teal-600 dark:text-teal-600'>What you can expect from me</h3>
           <div className='lg:flex gap-10'>  
             <div className='bg-white flex flex-col items-center text-current shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={design} width={100} height={100} alt='design'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful designs</h3>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-600'>Background</h3>
               <p className='py-2'>
-                Creating elegant designs suited for your needs.
+                Used to work with different cultures, behaviours and perspectives.
               </p>
-              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
-              <p className='text-gray-800 py-1 '>Photoshop</p>
-              <p className='text-gray-800 py-1 '>Illustrator</p>
-              <p className='text-gray-800 py-1 '>Figma</p>
+              <p className='py-2 '>Native Portuguese, fluent English and intermediate Italian</p>
+              <p className='py-2 '>Adaptability</p>
+              <p className='py-2 '>Increased creativity</p>
             </div>
             <div className='bg-white flex flex-col items-center text-current shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={code} width={100} height={100} alt='design'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful designs</h3>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-600'>Hard skills</h3>
               <p className='py-2'>
-                Creating elegant designs suited for your needs.
+                Programming logic
               </p>
-              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
-              <p className='text-gray-800 py-1 '>Photoshop</p>
-              <p className='text-gray-800 py-1 '>Illustrator</p>
-              <p className='text-gray-800 py-1 '>Figma</p>
+              <p className='py-2 '>Diversity in thought processes and problem-solving </p>
+              <p className='py-2 '>Knowledge in software design patterns, REST API and SOLID principles</p>
+              <p className='py-2 '>Experience developing front-end, back-end and full stack applications</p>
             </div>
             <div className='bg-white flex flex-col items-center text-current shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               <Image src={consulting} width={100} height={100} alt='design'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful designs</h3>
+              <h3 className='text-lg font-medium pt-8 pb-2 text-teal-600'>Soft skills</h3>
               <p className='py-2'>
-                Creating elegant designs suited for your needs.
+                Clear, effective communication and active listening
               </p>
-              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
-              <p className='text-gray-800 py-1 '>Photoshop</p>
-              <p className='text-gray-800 py-1 '>Illustrator</p>
-              <p className='text-gray-800 py-1 '>Figma</p>
+              <p className='py-2 '>Critical thinking</p>
+              <p className='py-2 '>Teamwork, collaboration and conflict resolution</p>
+              <p className='py-2 '>Attention to detail</p>
+              <p className='py-2 '>Continuous and easy learning experience</p>
             </div>
           </div>
         </section>
         <section>
           <div className='text-center'>
-            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <h3 className='text-3xl py-1 text-teal-600'>Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I have done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
-              Yeah yeah I am just going to write a lot of random things in here because apparently my lorem is not working so I have to do it manually.
+              Below you can find some of my projects, including front-end only, back-end only and full stack ones. When available, you can access both the deployed website and the GitHub repository.
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-          <div className='bg-white p-5 rounded-lg basis-1/3 flex-1 flex-col items-center justify-center'>
+          <div className='bg-white p-5 rounded-lg basis-1/3 flex-1 flex-col items-center flex'>
+            <h1 className='mb-3'>Task list</ h1>
             <Image src={tasklist} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive' alt='image'/>
              <div className='flex justify-evenly mt-3 w-full'>
               <a href='https://github.com/jpastolfi/to-do-list-crud' target="_blank" className='bg-teal-600 text-white font-bold py-2 px-4 rounded'>Github</a>
